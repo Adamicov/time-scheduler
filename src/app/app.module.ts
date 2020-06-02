83d923e3e8d3a0c62890c3ed053b5165fef834d0
Adamicov <Adam.Weberit@gmail.com>
2020-06-02 20:53:40 +0200

    Initial commit with config and ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CoreModule} from '@core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        CoreModule,
+        AppRoutingModule,
+        StoreModule.forRoot({}, {})
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
