25f33b61d1c287820d166cb2fbc0340585d62ef8
Adamicov <Adam.Weberit@gmail.com>
2020-06-02 21:47:23 +0200

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {CoreModule} from '@core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { StoreModule } from '@ngrx/store';
         BrowserModule,
         CoreModule,
         AppRoutingModule,
-        StoreModule.forRoot({}, {})
+        StoreModule.forRoot({}, {}),
+        BrowserAnimationsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
